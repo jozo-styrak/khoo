13715199bee59649782a782e6bae063ef5dabc6a
Jozef Štyrák <jozef.styrak@gmail.com>
2014-05-28 15:57:59 +0200

    updated brat formatting

--- src/main/java/ntnu/khoo/patterns/output/BratOutput.java
+++ src/main/java/ntnu/khoo/patterns/output/BratOutput.java
@@ -20,15 +20,19 @@ public class BratOutput extends Output {
 	
 	@Override
 	public void printRelation(SentenceWrapper sentence, List<Tree> cause, List<Tree> effect) {
+		
+		List<Tree> causeAdj = (cause.get(0).label().toString().compareTo(".") == 0) ? cause.subList(1, cause.size()) : cause;
+		List<Tree> effectAdj = (effect.get(0).label().toString().compareTo(".") == 0) ? effect.subList(1, effect.size()) : effect;
+		
 		// create strings from tree objects
-		String causeString = this.treeListToString(cause);
-		String effectString = this.treeListToString(effect);
+		String causeString = this.treeListToString(causeAdj);
+		String effectString = this.treeListToString(effectAdj);
 		
 		// get positions
-		int causeOffsetStart = sentence.getSentencePosition(cause.get(0)) + this.getOffset(sentence, cause.get(0));
-		int causeOffsetEnd = sentence.getSentencePosition(cause.get(cause.size()-1)) + this.getOffset(sentence, cause.get(cause.size()-1)) + cause.get(cause.size()-1).label().toString().length();
-		int effectOffsetStart = sentence.getSentencePosition(effect.get(0)) + this.getOffset(sentence, effect.get(0));
-		int effectOffsetEnd = sentence.getSentencePosition(effect.get(effect.size()-1)) + this.getOffset(sentence, effect.get(effect.size()-1)) + effect.get(effect.size()-1).label().toString().length();
+		int causeOffsetStart = sentence.getSentencePosition(causeAdj.get(0)) + this.getOffset(sentence, causeAdj.get(0));
+		int causeOffsetEnd = sentence.getSentencePosition(causeAdj.get(causeAdj.size()-1)) + this.getOffset(sentence, causeAdj.get(causeAdj.size()-1)) + causeAdj.get(causeAdj.size()-1).label().toString().length();
+		int effectOffsetStart = sentence.getSentencePosition(effectAdj.get(0)) + this.getOffset(sentence, effectAdj.get(0));
+		int effectOffsetEnd = sentence.getSentencePosition(effectAdj.get(effectAdj.size()-1)) + this.getOffset(sentence, effectAdj.get(effectAdj.size()-1)) + effectAdj.get(effectAdj.size()-1).label().toString().length();
 		
 		// print out
 		this.out.println("T" + this.phraseIndex  + "\tCause " + causeOffsetStart + " " + causeOffsetEnd + "\t" + causeString);
@@ -46,6 +50,8 @@ public class BratOutput extends Output {
 		List<Tree> leaves = sentence.getRootTree(leaf).getLeaves();
 		
 		int i = (leaves.get(0).label().toString().compareTo(".") == 0) ? 1 : 0;
+//		int count = (leaves.get(0).label().toString().compareTo(".") == 0) ? -2 : 0;
+//		int i = 0;
 		boolean found = false;
 		
 		while (i < leaves.size() && !found) {
